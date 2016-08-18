39f3918cb859f5b8934d4201618f8919e8da9b26
Nunes Thomas <thnunes@pc.in.ac-grenoble.fr>
2016-08-18 15:20:49 +0200

    langs

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -4,10 +4,33 @@
 function MainCtrl() {
 	
 	this.test = "HOHO"
+	this.currentLang= "FR"
+	this.langs = ["fr", "pt", "cr", "br", "en", "kl"]
+	var self = this
 	
+	/** */
     this.switchLang = function(lang){
-
+		console.log(lang)
+		self.currentLang = lang
+		
+		this.hideAllLang()
+		this.showLang(lang)
     }
+	
+	/** */
+	this.hideAllLang = function() {
+		[].forEach.call(self.langs, function(lang) {
+		   var els = document.getElementsByClassName(lang);
+		   [].forEach.call(els, function(el) {el.style.display = 'none' });
+		})
+	}
+	
+	/** */
+	this.showLang = function(lang) {
+	   var els = document.getElementsByClassName(lang);
+	   [].forEach.call(els, function(el) {el.style.display = 'block' });
+	}
+	
 }
 MainCtrl.$inject = []
 
