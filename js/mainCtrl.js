bc98d439c230b2bddeff6bb397c747812abe3d08
Nunes Thomas <thnunes@pc.in.ac-grenoble.fr>
2016-08-25 14:37:21 +0200

    countdown + bug mobile

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,12 +1,47 @@
 'use strict'
 
 //////////////////////////////////////CONTROLLER///////////////////////////////
-function MainCtrl() {
+function MainCtrl($interval) {
+
+	// Functions ********************************************************************
+	/** */
+    this.updateCountDown = function(dateNow){
 	
-	this.test = "HOHO"
-	this.currentLang= "FR"
-	this.langs = ["fr", "pt", "cr", "br", "en", "kl"]
-	var self = this
+		var dateNow = new Date()
+		
+		if (dateNow.getMilliseconds() > self.wedDate.getMilliseconds()) {
+	
+			self.monthCd = self.wedDate.getMonth() - dateNow.getMonth()
+			self.dayCd = self.wedDate.getDay() - dateNow.getDay()
+			self.hourCd = self.wedDate.getHours() - dateNow.getHours()
+			self.minCd = self.wedDate.getMinutes() - dateNow.getMinutes()
+			self.secCd = self.wedDate.getSeconds() - dateNow.getSeconds()
+	
+			if (self.secCd<0) {
+				self.secCd+=60
+				self.minCd--
+			}
+			
+			if (self.minCd<0) {
+				self.minCd+=60
+				self.hourCd--
+			}
+			
+			if (self.hourCd<0) {
+				self.hourCd+=24
+				self.dayCd--
+			}
+			
+			if (self.dayCd<0) {
+				self.dayCd+=dateNow.monthDays()
+				self.monthCd--
+			}
+			
+			if (self.monthCd<0) {
+				self.monthCd+=12
+			}
+		}
+    }
 	
 	/** */
     this.switchLang = function(lang){
@@ -30,11 +65,34 @@ function MainCtrl() {
 	   var els = document.getElementsByClassName(lang);
 	   [].forEach.call(els, function(el) {el.style.display = 'block' });
 	}
+		
+	// Attributes
+	
+	this.test = "HOHO"
+	this.currentLang = "FR"
+	this.wedDate = new Date(2017, 8-1, 24, 13, 0, 42, 42)
+	this.langs = ["fr", "pt", "cr", "br", "en", "kl", "el"]
+	var self = this
+	
+	self.monthCd = 0
+	self.dayCd = 0
+	self.hourCd = 0
+	self.minCd = 0
+	self.secCd = 0
+	
+	// Init
+	this.updateCountDown()
+	 $interval(this.updateCountDown, 1000);
 	
 }
-MainCtrl.$inject = []
+MainCtrl.$inject = ['$interval']
 
 ////////////////////////////////// APP DEFINITION ///////////////////////////////
 angular
     .module('MyPage', [])
     .controller('MainCtrl', MainCtrl);
+	
+Date.prototype.monthDays= function(){
+    var d= new Date(this.getFullYear(), this.getMonth()+1, 0);
+    return d.getDate();
+}
